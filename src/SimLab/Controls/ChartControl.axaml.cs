00f40bcbc5af7f0642b6c85d1a444a499f7a54bf
radekbocon <86799925+radekbocon@users.noreply.github.com>
2026-05-24 07:40:03 +0200

    Crosshair dynamic colors

--- src/SimLab/Controls/ChartControl.axaml.cs
+++ src/SimLab/Controls/ChartControl.axaml.cs
@@ -1,9 +1,15 @@
 using System;
 using System.Collections;
+using System.Linq;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml.Templates;
+using Avalonia.Media;
+using LiveChartsCore.Drawing;
+using LiveChartsCore.Painting;
 using LiveChartsCore.SkiaSharpView.Avalonia;
+using LiveChartsCore.SkiaSharpView.Painting;
+using SkiaSharp;
 
 namespace SimLab.Controls;
 
@@ -31,6 +37,18 @@ public partial class ChartControl : UserControl
 
     public static readonly StyledProperty<double?> YMinStepProperty =
         AvaloniaProperty.Register<ChartControl, double?>(nameof(YMinStep));
+
+    public static readonly StyledProperty<IBrush> CrosshairLabelsBackgroundProperty =
+        AvaloniaProperty.Register<ChartControl, IBrush>(nameof(CrosshairLabelsBackground),
+            new SolidColorBrush(Color.FromRgb(0x66, 0x66, 0x66)));
+
+    public static readonly StyledProperty<IBrush> CrosshairLabelsPaintProperty =
+        AvaloniaProperty.Register<ChartControl, IBrush>(nameof(CrosshairLabelsPaint),
+            new SolidColorBrush(Color.FromRgb(0xEE, 0xEE, 0xEE)));
+
+    public static readonly StyledProperty<IBrush> CrosshairPaintProperty =
+        AvaloniaProperty.Register<ChartControl, IBrush>(nameof(CrosshairPaint),
+            new SolidColorBrush(Color.FromRgb(0x99, 0x99, 0x99)));
 
     public string Title
     {
@@ -74,8 +92,79 @@ public partial class ChartControl : UserControl
         set => SetValue(SeriesTemplateProperty, value);
     }
 
+    public IBrush CrosshairLabelsBackground
+    {
+        get => GetValue(CrosshairLabelsBackgroundProperty);
+        set => SetValue(CrosshairLabelsBackgroundProperty, value);
+    }
+
+    public IBrush CrosshairLabelsPaint
+    {
+        get => GetValue(CrosshairLabelsPaintProperty);
+        set => SetValue(CrosshairLabelsPaintProperty, value);
+    }
+
+    public IBrush CrosshairPaint
+    {
+        get => GetValue(CrosshairPaintProperty);
+        set => SetValue(CrosshairPaintProperty, value);
+    }
+
+    static ChartControl()
+    {
+        CrosshairLabelsBackgroundProperty.Changed.AddClassHandler<ChartControl>(
+            (control, e) => control.OnCrosshairStyleChanged());
+        CrosshairLabelsPaintProperty.Changed.AddClassHandler<ChartControl>(
+            (control, e) => control.OnCrosshairStyleChanged());
+        CrosshairPaintProperty.Changed.AddClassHandler<ChartControl>(
+            (control, e) => control.OnCrosshairStyleChanged());
+    }
+
     public ChartControl()
     {
         InitializeComponent();
+        UpdateCrosshairStyles();
+    }
+
+    private void OnCrosshairStyleChanged()
+    {
+        UpdateCrosshairStyles();
+    }
+
+    private void UpdateCrosshairStyles()
+    {
+        if (ChartElement.XAxes.FirstOrDefault() is not XamlAxis xAxis) return;
+        if (ChartElement.YAxes.FirstOrDefault() is not XamlAxis yAxis) return;
+
+        xAxis.CrosshairLabelsBackground = BrushToLvcColor(CrosshairLabelsBackground);
+        yAxis.CrosshairLabelsBackground = BrushToLvcColor(CrosshairLabelsBackground);
+
+        xAxis.CrosshairLabelsPaint = BrushToPaint(CrosshairLabelsPaint);
+        yAxis.CrosshairLabelsPaint = BrushToPaint(CrosshairLabelsPaint);
+
+        xAxis.CrosshairPaint = BrushToPaint(CrosshairPaint);
+        yAxis.CrosshairPaint = BrushToPaint(CrosshairPaint);
+    }
+
+    private static Paint? BrushToPaint(IBrush? brush)
+    {
+        if (brush is ISolidColorBrush solid)
+        {
+            var c = solid.Color;
+            return new SolidColorPaint(new SKColor(c.R, c.G, c.B, c.A));
+        }
+
+        return null;
+    }
+    
+    private static LvcColor? BrushToLvcColor(IBrush? brush)
+    {
+        if (brush is ISolidColorBrush solid)
+        {
+            var c = solid.Color;
+            return new LvcColor(c.R, c.G, c.B, c.A);
+        }
+
+        return null;
     }
 }
