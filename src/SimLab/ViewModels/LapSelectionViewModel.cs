95cf02203808eda5e5ae9986d4d4a89dc0242187
radekbocon <86799925+radekbocon@users.noreply.github.com>
2026-05-25 20:43:01 +0200

    Cleanup

--- src/SimLab/ViewModels/LapSelectionViewModel.cs
+++ src/SimLab/ViewModels/LapSelectionViewModel.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
@@ -10,51 +9,6 @@ using SimLab.Models;
 using SimLab.Services;
 
 namespace SimLab.ViewModels;
-
-public partial class SelectableLap : ObservableObject
-{
-    private bool _isSelected;
-
-    public Lap Lap { get; }
-    public Session Session { get; }
-    public string DisplayName => $"Lap {Lap.Number} ({Lap.Time:mm\\:ss\\.fff})";
-    public bool IsValid => Lap.IsValid;
-
-    public bool IsSelected
-    {
-        get => _isSelected;
-        set => SetProperty(ref _isSelected, value);
-    }
-
-    public SelectableLap(Lap lap, Session session)
-    {
-        Lap = lap;
-        Session = session;
-    }
-}
-
-public partial class SelectableSession : ObservableObject
-{
-    private bool _isExpanded = true;
-
-    public Session Session { get; }
-    public ObservableCollection<SelectableLap> Laps { get; }
-    public string DisplayName => $"{Session.Info.Car} at {Session.Info.Track} - {Session.Info.StartTime:g}";
-    public int LapCount => Laps.Count;
-
-    public bool IsExpanded
-    {
-        get => _isExpanded;
-        set => SetProperty(ref _isExpanded, value);
-    }
-
-    public SelectableSession(Session session)
-    {
-        Session = session;
-        Laps = new ObservableCollection<SelectableLap>(
-            session.Laps.Select(l => new SelectableLap(l, session)));
-    }
-}
 
 public partial class LapSelectionViewModel : ObservableObject
 {
@@ -99,14 +53,14 @@ public partial class LapSelectionViewModel : ObservableObject
 
             foreach (var lap in selectableSession.Laps)
             {
-                lap.PropertyChanged += Lap_PropertyChanged;
+                lap.PropertyChanged += LapOnPropertyChanged;
             }
             
             Sessions.Add(selectableSession);
         }
     }
 
-    private void Lap_PropertyChanged(object? sender, PropertyChangedEventArgs e)
+    private void LapOnPropertyChanged(object? sender, PropertyChangedEventArgs e)
     {
         if (e.PropertyName == nameof(SelectableLap.IsSelected))
         {
@@ -146,5 +100,40 @@ public partial class LapSelectionViewModel : ObservableObject
     private void Cancel()
     {
         DialogHost.Close(null);
+    }
+}
+
+public partial class SelectableLap : ObservableObject
+{
+    public Lap Lap { get; }
+    public Session Session { get; }
+    public string DisplayName => $"Lap {Lap.Number} ({Lap.Time:mm\\:ss\\.fff})";
+    public bool IsValid => Lap.IsValid;
+
+    [ObservableProperty]
+    public partial bool IsSelected { get; set; }
+
+    public SelectableLap(Lap lap, Session session)
+    {
+        Lap = lap;
+        Session = session;
+    }
+}
+
+public partial class SelectableSession : ObservableObject
+{
+    public Session Session { get; }
+    public ObservableCollection<SelectableLap> Laps { get; }
+    public string DisplayName => $"{Session.Info.Car} at {Session.Info.Track} - {Session.Info.StartTime:g}";
+    public int LapCount => Laps.Count;
+
+    [ObservableProperty]
+    public partial bool IsExpanded { get; set; }
+
+    public SelectableSession(Session session)
+    {
+        Session = session;
+        Laps = new ObservableCollection<SelectableLap>(
+            session.Laps.Select(l => new SelectableLap(l, session)));
     }
 }
